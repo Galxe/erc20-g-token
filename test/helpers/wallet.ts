2fbfe3f0e6dd2d93683e45d778101d7958b82d7f
tobin <136327983+DepingMeng@users.noreply.github.com>
2024-05-09 23:12:30 +0800

    test: add test (#2)
    Co-authored-by: Yumin Xia <me@yumin-xia.com>

--- test/helpers/wallet.ts
+++ test/helpers/wallet.ts
@@ -1,14 +1,14 @@
-import { ethers } from "hardhat";
 import { HDNodeWallet } from "ethers";
+import { ethers } from "hardhat";
 
 // Generates random wallet and connects it to hardhat provider, sets balance to 100 ETH
 export async function generateRandomWallet(): Promise<HDNodeWallet> {
-   // Connect to Hardhat Provider
-   const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
-   // Set balance
-   await ethers.provider.send("hardhat_setBalance", [
-       wallet.address,
-       "0x56BC75E2D6310000000", // 10000 ETH
-   ]);
-   return wallet;
+  // Connect to Hardhat Provider
+  const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+  // Set balance
+  await ethers.provider.send("hardhat_setBalance", [
+    wallet.address,
+    "0x56BC75E2D6310000000", // 10000 ETH
+  ]);
+  return wallet;
 }
