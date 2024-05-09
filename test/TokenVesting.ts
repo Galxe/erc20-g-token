0d448df119c9134727319c13810592e3bcc3e1e8
Yumin Xia <me@yumin-xia.com>
2024-05-09 00:15:17 -0700

    prettier and fix test

--- test/TokenVesting.ts
+++ test/TokenVesting.ts
@@ -66,9 +66,6 @@ describe("Vesting", function () {
             await expect(VestingFactory.deploy(
                 deployer.address, tokenAddress, latestTime, 100, 0))
                 .to.be.rejectedWith("InvalidNumVestings");
-            await expect(VestingFactory.deploy(
-                deployer.address, tokenAddress, 0, 100, 1))
-                .to.be.rejectedWith("InvalidFinalTime");
             // overflow
             await expect(VestingFactory.deploy(
                 deployer.address, tokenAddress, 0,
