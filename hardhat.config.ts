1535d6a006d2407ac3f3eafb6fb0b08478571b69
Yumin Xia <me@yumin-xia.com>
2024-05-12 21:55:55 -0700

    prepare deployment scripts (#4)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,71 @@
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-solhint";
 import { HardhatUserConfig } from "hardhat/config";
+import { vars } from "hardhat/config";
+
+// explorer api keys
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY", "");
+const BSCSCAN_API_KEY = vars.get("BSCSCAN_API_KEY", "");
+const BASESCAN_API_KEY = vars.get("BASESCAN_API_KEY", "");
+
+// deployer keys
+const DEPLOYER_PRIVATE_KEY = vars.get("GALXE_DEPLOYER_PRIVATE_KEY", "ff".repeat(32));
+const accounts = [DEPLOYER_PRIVATE_KEY];
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
-  ignition: {
-    strategyConfig: {
-      create2: {
-        salt: "0x0000000000000000000000000000000000000000000000000000000000000000",
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 2000,
       },
     },
+  },
+  // we don't directly use ignition, instead, they are used in scripts.
+  // ignition: {
+  //   strategyConfig: {
+  //     create2: {
+  //       salt: salt,
+  //     },
+  //   },
+  // },
+  defaultNetwork: "hardhat",
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+      mainnet: ETHERSCAN_API_KEY,
+      bsc: BSCSCAN_API_KEY,
+      base: BASESCAN_API_KEY,
+    },
+  },
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "https://rpc.ankr.com/eth_sepolia",
+      chainId: 11155111,
+      accounts,
+    },
+    bsc: {
+      url: "https://bsc-dataseed1.binance.org",
+      chainId: 56,
+      accounts,
+    },
+    polygon: {
+      url: "https://rpc.ankr.com/polygon",
+      chainId: 137,
+      accounts,
+    },
+    mainnet: {
+      url: "https://cloudflare-eth.com",
+      chainId: 1,
+      accounts,
+    },
+    base: {
+      url: "https://mainnet.base.org",
+      chainId: 8453,
+      accounts
+    }
   },
 };
 
---
+++ ignition/modules/TokenUpgrader.ts
@@ -0,0 +1,19 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const TokenUpgraderModule = buildModule("TokenUpgraderModule", (m) => {
+  const deployer = m.getParameter("deployer");
+  const galAddr = m.getParameter("gal_address");
+  const gAddr = m.getParameter("g_address");
+  const multiSigAddr = m.getParameter("multi_sig");
+
+  // initialize upgrader with chain-specific values
+  const upgrader = m.contract("TokenUpgrader", [deployer]);
+  m.call(upgrader, "initialize", [galAddr, gAddr]);
+
+  // transfer ownerships
+  m.call(upgrader, "transferOwnership", [multiSigAddr]);
+
+  return { upgrader };
+});
+
+export default TokenUpgraderModule;
