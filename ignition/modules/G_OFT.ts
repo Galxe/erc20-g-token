c092e4e1e4fab703a5edbaa735b55bfd797bf716
oyyblin <4529189+oyyblin@users.noreply.github.com>
2024-06-12 16:35:52 -0700

    feat: support layerzero oft standard

---
+++ ignition/modules/G_OFT.ts
@@ -0,0 +1,16 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const G_OFTModule = buildModule("G_OFT", (m) => {
+  const lzEndpointAddr = m.getParameter("lz_endpoint");
+  const ownerAddr = m.getParameter("owner");
+
+  const gOFT = m.contract("G_OFT", [
+    "GravityTokenG", // name
+    "G", // symbol
+    lzEndpointAddr, // lzEndpoint
+    ownerAddr, // delegate
+  ]);
+  return { gOFT };
+});
+
+export default G_OFTModule;
